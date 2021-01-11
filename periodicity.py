dc4d989d00bb5da36ef38619482b07f5f5dcf9dd
GitTomRepo <68048334+GitTomRepo@users.noreply.github.com>
2021-01-11 19:12:28 +0100

    Create periodicity.py

---
+++ periodicity.py
@@ -0,0 +1,18 @@
+# PYTHON_3
+# Author : GitTomRepo
+# Link :
+
+number = int(input("Number : "))
+diviseur = int(input("Divider : "))
+valeur_ref = number % diviseur
+valeur_post = number**2 % diviseur
+
+i = 3
+
+while valeur_post != valeur_ref :
+    valeur_post = number**i % diviseur
+    i += 1
+    print(valeur_post)
+
+p = i-2
+print("Periodicite : ", p)
